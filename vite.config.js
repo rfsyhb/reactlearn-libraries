5b1c9874340c428f8586df3659075e23188b3267
Rafi Syihab <rafisyihab3@gmail.com>
2024-10-17 15:07:39 +0700

    (development) create proxy to mockapi

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://666d2bf37a3738f7cacbadeb.mockapi.io/api/v1/',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // menghilangkan '/api' dari path
+      },
+    },
+  },
+});
